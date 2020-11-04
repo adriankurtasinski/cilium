ea9ff1fabd1d6d251d0ca5800a343462a8bf0012
Robin Hahling <robin.hahling@gw-computing.net>
2020-11-04 14:50:49 +0100

    daemon: introduce new flag to specify Hubble events buffer capacity
    Mark the old flag as deprecated as well.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -844,8 +844,12 @@ func init() {
 	flags.StringSlice(option.HubbleTLSClientCAFiles, []string{}, "Paths to one or more public key files of client CA certificates to use for TLS with mutual authentication (mTLS). The files must contain PEM encoded data. When provided, this option effectively enables mTLS.")
 	option.BindEnv(option.HubbleTLSClientCAFiles)
 
-	flags.Int(option.HubbleFlowBufferSize, 4095, "Maximum number of flows in Hubble's buffer. The actual buffer size gets rounded up to the next power of 2, e.g. 4095 => 4096")
+	flags.Int(option.HubbleFlowBufferSize, 0, "Maximum number of flows in Hubble's buffer.")
+	flags.MarkDeprecated(option.HubbleFlowBufferSize, fmt.Sprintf("Use %s instead.", option.HubbleEventBufferCapacity))
 	option.BindEnv(option.HubbleFlowBufferSize)
+
+	flags.Int(option.HubbleEventBufferCapacity, 4095, "Capacity of Hubble events buffer. The provided value MUST satisfy n=2^i -1 for i = [1, 16] (ie: 1, 3, ..., 2047, 4095, ..., 65535)")
+	option.BindEnv(option.HubbleEventBufferCapacity)
 
 	flags.Int(option.HubbleEventQueueSize, 0, "Buffer size of the channel to receive monitor events.")
 	option.BindEnv(option.HubbleEventQueueSize)
